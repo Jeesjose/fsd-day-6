31ae38f2bf64af596b69d3aaa509eb443aeb6b1d
jithin4645 <jithinnath14@gmail.com>
2025-04-09 16:20:54 +0530

    dotenv

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-var express = require("express")
-require('./db')
+var express = require('express');
+var dotenv = require('dotenv');
 var userRoutes = require('./route/userroute')
 var pRoutes = require('./route/productroute')
  var app = express();
+ 
+
+ dotenv.config();
+ require('./db');
+ var port = process.env.port
+
  app.use(express.json());
-
- var port =3001;
-
-
-
 
 app.use('/api',userRoutes)
 app.use('/api1',pRoutes)
--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://thanikkaljees2020:jeesjose@cluster0.dakkqtq.mongodb.net/assement?retryWrites=true&w=majority&appName=Cluster0")
+//mongoose.connect("mongodb+srv://thanikkaljees2020:jeesjose@cluster0.dakkqtq.mongodb.net/assement?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect(process.env.mongodb_url)
 .then(()=>{
     console.log("connected to DB")
 
